b9133a0e1b0c667e1e21d2e7868838338ad25650
Avihay-Laptop <avihay30@gmail.com>
2021-09-23 14:16:32 +0300

    added styling and some mock data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FlightsService } from './flights.service';
+import { Flight } from '../flight.model';
+import { FlightsService } from '../flights.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  flights: Flight[] = [];
 
-  constructor(private flightsService: FlightsService) { }
+  constructor(private flightsService: FlightsService) {}
 
   ngOnInit(): void {
-  }
-
-  getFlights() {
-    
+    this.flights = this.flightsService.getFlights();
   }
 }
