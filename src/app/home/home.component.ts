1e6dc393e3fb4f10decf7cf194ba80062bdad887
Avihay-Laptop <avihay30@gmail.com>
2021-09-26 02:20:54 +0300

    added some styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,38 +8,53 @@ import { FlightsService } from '../flights.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  static readonly selectedDefault = 'All';
+
   flights!: Flight[];
-  filteredOriginList!: any[];
-  filteredDestinationList!: any[];
+  filteredOriginList: string[];
+  filteredDestinationList: string[];
   selectedOrigin!: string;
   selectedDestination!: string;
 
-  constructor(private flightsService: FlightsService) {}
+  constructor(private flightsService: FlightsService) {
+    this.selectedOrigin = this.selectedDestination = HomeComponent.selectedDefault;
+    this.filteredOriginList = this.filteredDestinationList = [
+      HomeComponent.selectedDefault,
+    ];
+  }
 
   ngOnInit(): void {
     this.flightsService.getAllFlights().subscribe((data: Flight[]) => {
       this.flights = data;
     });
 
-    this.flightsService.getAllOrigins().subscribe((data: string[]) => {
-      this.filteredOriginList = data;
+    this.flightsService.getAllOrigins().subscribe((data: Flight[]) => {
+      const origins = data.map((flight: Flight) => flight.origin);
+      this.filteredOriginList = this.filteredOriginList.concat(origins);
     });
 
-    this.flightsService.getAllDestinations().subscribe((data: string[]) => {
-      this.filteredDestinationList = data;
+    this.flightsService.getAllDestinations().subscribe((data: Flight[]) => {
+      const destinations = data.map((flight: Flight) => flight.destination);
+      this.filteredDestinationList = this.filteredDestinationList.concat(destinations);
     });
   }
 
+  // TBD: add query for all to one and vice versa
   query(): void {
+    const selectedDefault = HomeComponent.selectedDefault;
     const origin = this.selectedOrigin;
     const destination = this.selectedDestination;
-
-    if (origin && destination) {
+    const isNotEmpty = origin && destination;
+    const isDefault = origin == selectedDefault && destination == selectedDefault;
+    if (isDefault) {
+      this.flightsService
+        .getAllFlights()
+        .subscribe((data: Flight[]) => (this.flights = data));
+    }
+    else if (isNotEmpty) {
       this.flightsService
         .getQueriedFlights(origin, destination)
-        .subscribe((data: Flight[]) => {
-          this.flights = data;
-        });
+        .subscribe((data: Flight[]) => (this.flights = data));
     }
   }
 }
